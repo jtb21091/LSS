aefd86981ad2913892794469c664280c3dfe19af
Josh Brooks <60986161+jtb21091@users.noreply.github.com>
2024-11-25 21:23:43 -0500

    Update index.js

--- AWS/src/index.js
+++ AWS/src/index.js
@@ -1,8 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import './index.css'; // Ensure this path points to the CSS file in the same directory
+import App from './App'; // Import App.js from the same directory
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
